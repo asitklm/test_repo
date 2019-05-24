7a9fc9eceada349ed3bccdb6c0a4ae21c2835168
asitklm <50411766+asitklm@users.noreply.github.com>
2019-05-24 05:34:01 -0400

    Add files via upload

---
+++ databricks_RCC.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+dbutils.fs.ls("/mnt/FIONA/RDS/Ahold/Item_Cost/Ad_Wk")
+#dbutils.fs.mount("/mnt/FIONA/RDS/Ahold/Item_Cost/Ad_Wk/20190131_stsh_cost_ad1.txt", "/mnt/")
+now =datetime.now()
+
+file_ =  open("/dbfs/mnt/FIONA/RDS/Ahold/Item_Cost/Ad_Wk/20190131_stsh_cost_ad1.txt", 'r')
+
+counter = 0
+for _ in file_:
+    counter = counter + 1
+    
+end = datetime.now()
+print("counter:")
+print(counter)
+print(str(end-now))
+
+file.close()
